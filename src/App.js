05cf513795c705d101b947c26aa368a2426c7cd9
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-12-06 15:45:26 -0500

    Login component created

--- src/App.js
+++ src/App.js
@@ -7,71 +7,78 @@ import RegisterForm from './Register/RegisterForm';
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      loggedInUser: null,
-
-    }
+      super(props);
+      this.state = { 
+          loggedInUser: null,
+         
+      };
   }
+
+  registerNewUser = async (user) => {
+      console.log("User object from Register: ", user)
+      try{
+          const response = await axios.post('https://localhost:44394/api/authentication', user);
+          console.log(response)
+          this.loggedInUser = ({'userName': user.userName, 'password': user.password})
+
+      }
+      catch(error) {
+          console.log(error, 'Invalid input');
+      }
+  }
+
+  componentDidMount() {
+      const jwt = localStorage.getItem('token');
+      try{
+          const user = jwt_decode(jwt);
+          this.setState({
+              user
+          });
+      } catch (err) {
+         console.log(err) 
+      }
+      this.getProduct()
+  }
+  
+  async makeLoginRequest(logInfo){
+      try{
+
+          let userInfo = logInfo;
+          let response = await axios.post('https://localhost:44394/api/authentication/login', userInfo);
+
+          localStorage.setItem('token', response.data.token);
+          window.location = '/';
+          return localStorage;
+      }
+      catch(ex){
+          console.log("bad call")
+      }
+  } 
+  render(){
+    const user = this.state.user;
+    return(
+        <div>
+           <div>
+                <NavBar user={user} />
+                <Routes>
+                    <Route path='/register' element={<RegisterForm />} /> 
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/logout' element={<Logout />} />
+                    <Route path='/buddy' element={<Buddy/>} />
+                    <Route path='/tutorials' element={<Tutorials/>} /> 
+                </Routes>
+                    
+                  
+            </div>
+        </div>
+
+    )
 }
-
-registerNewUser = async (user) => {
-  consoNamele.log("User object from Register:", user)
-  try{
-    const response = await axios.post('', user);
-    console.log(response)
-    this.loggedInUser = ({'userName': userName, 'password': user.password})
-  }
-  catch(error) {
-    console.log(error, 'Invalid input');
-  }
 }
+                    
+                    
+                    
 
-componentDidMount() {
-  const jwt = localStorage.getItem('token');
-  try{
-    const user = jwt_decode(jwt);
-    this.setState({
-      user
-    });
-  } catch (err){
-    console.log(err)
-  }
-  this.getProduct()
-}
-
-async makeLoginRequest(logInfo){
-  try{
-
-    let userInfo = logInfo;
-    let response = await axios.post('', userInfo);
-
-    localStorage.setItem('token', response.data.token);
-    window.location = '/';
-    return localStorage;
-  }
-  catch(ex){
-    console.log("bad call")
-  }
-}
-
-render(){
-  const user = this.state.user;
-  return(
-    <div>
-      <div>
-        <NavBar user={user} />
-        <Routes>
-          <Route path='/register' element={<RegisterForm />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/logout' element={<Logout />} />
-          <Route path='buddy' element={<Buddy />} />
-          <Route path='tutorials' element={<Tutorials />} />
-        </Routes>
-      </div>
-    </div>
-  )
-}
 
 
 
